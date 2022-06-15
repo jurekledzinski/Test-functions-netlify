6244814b86a342315f5ae2cb034fbb86476a33ec
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2022-06-15 15:11:29 +0200

    Changes email

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -109,4 +109,11 @@ exports.handler = async function (event, context) {
       };
     }
   );
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      data: 'Send email',
+    }),
+  };
 };
