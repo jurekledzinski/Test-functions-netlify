bdfc2fe6a5bedce76502ac6a92406e4d3106d584
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2022-06-15 14:09:24 +0200

    Change body server message

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -4,9 +4,7 @@ exports.handler = async function (event, context) {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Hello World Server function netlify',
-      event: event,
-      context: context,
+      message: event.body,
     }),
   };
 };
