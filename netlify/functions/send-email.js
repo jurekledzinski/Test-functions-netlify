8621521a0b8b4a9ddc3e21682a6a2a25c12468b9
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2022-06-16 10:16:41 +0200

    Changes

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -1,7 +1,11 @@
 const nodemailer = require('nodemailer');
+const Email = require('../../models/email');
 
 exports.handler = async function (event) {
   const body = JSON.parse(event.body);
+
+  const email = await Email.create(body);
+  console.log(email, 'Email database');
 
   const output = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
   <html xmlns="http://www.w3.org/1999/xhtml" lang="en-GB">
