4cacf8b8b379fc4deedc7e17e7edcf826cffa6c3
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2022-06-16 11:59:31 +0200

    Changes

--- netlify/functions/get-emails.js
+++ netlify/functions/get-emails.js
@@ -8,7 +8,7 @@ exports.handler = async function (event) {
   console.log(mongoose.connection.readyState, 'Ready state get emails');
   const allEmails = await Email.find({});
   delete allEmails.__v;
-
+  console.log(allEmails);
   return {
     statusCode: 200,
     body: JSON.stringify({ value: allEmails }),
