88ec3df904a76b5ff4e741c1f507f5f2d0a2c38d
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2022-06-16 10:32:37 +0200

    Changes

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -8,8 +8,8 @@ exports.handler = async function (event) {
   console.log(mongoose);
   console.log(Email, 'Email');
 
-  //   const email = await Email.create(body);
-  //   console.log(email, 'Email database');
+  const email = await Email.create({ name: 'ok', email: 'em', message: 'msg' });
+  console.log(email, 'Email database');
   //   mongoose.connection.close();
 
   const output = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
