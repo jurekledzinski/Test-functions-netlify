4eba1addb66c24ae302e05c4ddd27bb9932a05d8
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2022-06-24 20:35:03 +0200

    Changes

--- netlify/functions/get-emails.js
+++ netlify/functions/get-emails.js
@@ -3,11 +3,11 @@ const connectDb = require('../../db/connectDb');
 const Email = require('../../models/email');
 
 connectDb(process.env.REACT_APP_DB);
-console.log(mongoose.connection.readyState, 'Ready state get emails outside');
+console.log(process, 'zmienne');
 
 exports.handler = async function (event) {
   console.log(mongoose.connection.readyState, 'Ready state get emails');
-  console.log(mongoose.connection.connectionStates, 'states connection');
+
   const allEmails = await Email.find({}).select('-__v');
   return {
     statusCode: 200,
