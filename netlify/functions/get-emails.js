8b3221b2556fa1f57d5948c41454b0297d37f5fa
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2022-06-24 22:21:07 +0200

    Changes

--- netlify/functions/get-emails.js
+++ netlify/functions/get-emails.js
@@ -7,6 +7,22 @@ connectDb(process.env.REACT_APP_DB);
 exports.handler = async function (event) {
   console.log(mongoose.connection.readyState, 'Ready state get emails');
 
+  mongoose.connection.on('connecting', function () {
+    console.log('Connecting setup handler...');
+  });
+
+  mongoose.connection.on('connected', function () {
+    console.log('Connected setup handler...');
+  });
+
+  mongoose.connection.on('error', function () {
+    console.log('Lost MongoDB connection error handler...');
+  });
+
+  mongoose.connection.on('reconnected', function () {
+    console.log('reconnected... handler');
+  });
+
   const allEmails = await Email.find({}).select('-__v');
   return {
     statusCode: 200,
